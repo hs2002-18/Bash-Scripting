089e5ca62a674d705eff742a82488dc96dafd979
hs2002-18 <harshshrimali319.com>
2026-03-16 18:08:35 +0000

    Updated

--- Day 3/error_handling.sh
+++ Day 3/error_handling.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+# Write a script that creates a directory given by the user.
 createDirectory() {
     mkdir $1  
 }
@@ -10,3 +12,24 @@ then
 fi
 
 echo "Directory $1 is created"
+
+# Write a script that checks if a file exists.
+
+fileExist() {
+    if [[ $# -eq 0 ]];
+    then
+        echo "No File Name Provided"
+        echo "Usage: $0 <filename>"
+        exit 1
+    fi
+    file=$1
+
+    if [[ -f "$file" ]];
+    then
+        echo "File Exists"
+    else
+        echo "File Not Found"
+    fi
+}
+
+fileExist "$1"
